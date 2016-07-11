5543b1cf1ad7d4f5de45e77901053ffb5703fdc2
Liam Naddell <beanboybunny@gmail.com>
2016-07-11 19:02:06 -0400

    update LDCSP

--- lnet/src/lnet/main.go
+++ lnet/src/lnet/main.go
@@ -1,61 +1,21 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
-	"time"
-)
+import "net"
+import "time"
+import "fmt"
 
 func main() {
-	conn, err := net.Dial("tcp", "skilstak.sh:8452")
-	fmt.Println("past uname")
+	conn, err := net.Dial("tcp", "skilstak.sh:8382")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("what is your nickname: ")
-	nname := bufio.NewScanner(os.Stdin)
-	nname.Scan()
-	conv(conn, nname.Text())
-	defer conn.Close()
-}
-func conv(conn net.Conn, nname string) {
-	RemAdr := conn.RemoteAddr()
-	fmt.Println("start talking: ")
-	//manage reads
-	go func() {
-		b := make([]byte, 1024)
-
-		for {
-			n, _ := conn.Read(b)
-			if n > 0 {
-				res := make([]byte, n)
-
-				copy(res, b[:n])
-				msgprnt(string(res))
-			}
-		}
-	}()
-	go func() {
-		NTEXT := bufio.NewScanner(os.Stdin)
-		for {
-			ntext := ""
-			NTEXT.Scan()
-			ntext = NTEXT.Text()
-			if ntext != "" {
-				conn.Write([]byte("[" + nname + "]" + ntext))
-			}
-			ntext = ""
-		}
-	}()
-	//echo -n "Old line"; echo -e "\e[<N>A new line"
-	for conn.RemoteAddr() == RemAdr {
+	fmt.Println("donepanik")
+	fmt.Println("writting")
+	_, err2 := conn.Write([]byte("shalmom"))
+	if err2 != nil {
+		panic(err)
+	}
+	for {
 		time.Sleep(1 * time.Second)
 	}
-	fmt.Println("closed")
-}
-
-func msgprnt(msg string) {
-	fmt.Println(msg)
 }
