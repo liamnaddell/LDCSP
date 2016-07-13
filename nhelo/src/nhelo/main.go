e4e2d06795aa6425eed00b0d9a54eb8906fe5519
Liam Naddell <beanboybunny@gmail.com>
2016-07-12 22:18:51 -0400

    Start broadcaster implementation

--- nhelo/src/nhelo/main.go
+++ nhelo/src/nhelo/main.go
@@ -2,17 +2,47 @@ package main
 
 import "net"
 import "time"
+import "fmt"
+
+var conns []net.Conn
 
 func main() {
 	ln, err := net.Listen("tcp", ":9000")
 	checkerr(err)
-	time.Sleep(4 * time.Second)
-	con1, err := ln.Accept()
-	time.Sleep(1 * time.Second)
-	b := make([]byte, 1024)
-	con1.Read(b)
-	con1.Write(b)
+
+	//accepter
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err == nil {
+				conns = append(conns, conn)
+			}
+		}
+	}()
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println(conns[0].LocalAddr())
+	go read()
+	for {
+		time.Sleep(1 * time.Second)
+	}
 }
+
+func read() {
+	for {
+		b := make([]byte, 1024)
+		for _, i := range conns {
+			n, _ := i.Read(b)
+			if n > 0 {
+				buf := make([]byte, n)
+				copy(buf, b[:n])
+				fmt.Println(string(buf))
+			}
+		}
+	}
+}
+
 func checkerr(err error) {
 	if err != nil {
 		panic(err)
